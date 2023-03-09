8479fb14a5a8d940d3ecf90a7f43c5c2111b70fb
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-09 10:26:32 -0500

    Set up basic infrastructure (cards, card-area, header, searchbar)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchbarComponent } from './searchbar/searchbar.component';
+import { CardAreaComponent } from './card-area/card-area.component';
+import { CardsComponent } from './card-area/cards/cards.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchbarComponent,
+    CardAreaComponent,
+    CardsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/card-area/card-area.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-area',
+  templateUrl: './card-area.component.html',
+  styleUrls: ['./card-area.component.css']
+})
+export class CardAreaComponent {
+
+}
