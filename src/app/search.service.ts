043e898ac052e5b8e3487ca391626bfeee9db111
Nixgit2000 <fourwaycoordination@gmail.com>
2023-03-10 15:10:05 -0500

    searchbar and buttons now work in separate components by overloading constructor with services

---
+++ src/app/search.service.ts
@@ -0,0 +1,81 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchService {
+
+  public allMoviesSearch = true
+  public fantasySearch = false
+  public actionSearch = false
+  public sciFiSearch = false
+  public comicBookSearch = false
+  public horrorSearch= false
+  public criticallyAcclaimedSearch = false
+
+  allMoviesFn() {
+    this.allMoviesSearch = true
+    this.fantasySearch = false
+    this.actionSearch = false
+    this.sciFiSearch = false
+    this.comicBookSearch = false
+    this.horrorSearch = false
+    this.criticallyAcclaimedSearch = false
+  }
+  fantasyFn() {
+    this.allMoviesSearch = false
+    this.fantasySearch = true
+    this.actionSearch = false
+    this.sciFiSearch = false
+    this.comicBookSearch = false
+    this.horrorSearch = false
+    this.criticallyAcclaimedSearch = false
+  }
+  actionFn() {
+    this.allMoviesSearch = false
+    this.fantasySearch = false
+    this.actionSearch = true
+    this.sciFiSearch = false
+    this.comicBookSearch = false
+    this.horrorSearch = false
+    this.criticallyAcclaimedSearch = false
+  }
+  sciFiFn() {
+    this.allMoviesSearch = false
+    this.fantasySearch = false
+    this.actionSearch = false
+    this.sciFiSearch = true
+    this.comicBookSearch = false
+    this.horrorSearch = false
+    this.criticallyAcclaimedSearch = false
+  }
+  comicBookFn() {
+    this.allMoviesSearch = false
+    this.fantasySearch = false
+    this.actionSearch = false
+    this.sciFiSearch = false
+    this.comicBookSearch = true
+    this.horrorSearch = false
+    this.criticallyAcclaimedSearch = false
+  }
+  horrorFn() {
+    this.allMoviesSearch = false
+    this.fantasySearch = false
+    this.actionSearch = false
+    this.sciFiSearch = false
+    this.comicBookSearch = false
+    this.horrorSearch = true
+    this.criticallyAcclaimedSearch = false
+  }
+  criticallyAcclaimedFn() {
+    this.allMoviesSearch = false
+    this.fantasySearch = false
+    this.actionSearch = false
+    this.sciFiSearch = false
+    this.comicBookSearch = false
+    this.horrorSearch = false
+    this.criticallyAcclaimedSearch = true
+  }
+
+
+}
--- src/app/searchbar/searchbar.component.ts
+++ src/app/searchbar/searchbar.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
-
+import { SearchService } from '../search.service'
+import { Search2Service } from '../search2.service'
+import { FormsModule } from '@angular/forms'
 @Component({
   selector: 'app-searchbar',
   templateUrl: './searchbar.component.html',
   styleUrls: ['./searchbar.component.css']
 })
 export class SearchbarComponent {
+ 
+  
+
+  constructor(public searching: SearchService, public searching2: Search2Service) {}
+
+ searchTextFn() {
+    this.searching2.searchTextFn()
+  }
+ 
+
+  allMoviesFn() {
+    this.searching.allMoviesFn()
+  }
+  fantasyFn() {
+    this.searching.fantasyFn()
+  }
+  actionFn() {
+    this.searching.actionFn()
+  }
+  sciFiFn() {
+    this.searching.sciFiFn()
+  }
+  comicBookFn() {
+    this.searching.comicBookFn()
+  }
+  horrorFn() {
+    this.searching.horrorFn()
+  }
+  criticallyAcclaimedFn() {
+    this.searching.criticallyAcclaimedFn()
+  }
+
 
 }
